9eb276a10e435a14a644667410f02deb6904b469
karthikv6 <44593407+karthikv6@users.noreply.github.com>
2023-05-14 07:51:28 -0700

    edited home_jv javascript to be more modular, easier to route
    slightly changed html to match

--- Front end/public/home_jv.js
+++ Front end/public/home_jv.js
@@ -1,51 +1,35 @@
-const editDialog1 = document.getElementById('edit_dialog_1');
-const editSupBtn1 = document.getElementById('edit_sup_1');
-const cancelBtn1 = document.getElementById('cancel_btn_1');
-const disBtn1 = document.getElementById('dis_sup_1');
-const saveBtn1 = document.getElementById('save_btn_1');
+window.addEventListener('DOMContentLoaded', (event) => {
+  let editButtons = document.querySelectorAll('[id^="edit_sup_"]');
+  let saveButtons = document.querySelectorAll('[id^="save_btn_"]');
+  let cancelButtons = document.querySelectorAll('[id^="cancel_btn_"]');
 
-editSupBtn1.addEventListener('click', () => {
-  editDialog1.showModal();
-});
-cancelBtn1.addEventListener('click', () => {
-  editDialog1.close();
-});
+  editButtons.forEach(button => {
+    let supplementNumber = button.getAttribute('data-supplement');
+    let dialog = document.querySelector(`#edit_dialog_${supplementNumber}`);
 
-const editDialog2 = document.getElementById('edit_dialog_2');
-const editSupBtn2 = document.getElementById('edit_sup_2');
-const cancelBtn2 = document.getElementById('cancel_btn_2');
-const disBtn2 = document.getElementById('dis_sup_2');
-const saveBtn2 = document.getElementById('save_btn_2');
+    button.addEventListener('click', () => {
+      dialog.showModal();
+    });
+  });
 
-editSupBtn2.addEventListener('click', () => {
-  editDialog2.showModal();
-});
-cancelBtn2.addEventListener('click', () => {
-  editDialog2.close();
-});
+  saveButtons.forEach(button => {
+    let supplementNumber = button.getAttribute('data-supplement');
+    let dialog = document.querySelector(`#edit_dialog_${supplementNumber}`);
 
-const editDialog3 = document.getElementById('edit_dialog_3');
-const editSupBtn3 = document.getElementById('edit_sup_3');
-const cancelBtn3 = document.getElementById('cancel_btn_3');
-const disBtn3 = document.getElementById('dis_sup_3');
-const saveBtn3 = document.getElementById('save_btn_3');
+    button.addEventListener('click', (event) => {
+      event.preventDefault();
+      // Do save logic here
+      dialog.close();
+    });
+  });
 
-editSupBtn3.addEventListener('click', () => {
-  editDialog3.showModal();
-});
-cancelBtn3.addEventListener('click', () => {
-  editDialog3.close();
-});
+  cancelButtons.forEach(button => {
+    let supplementNumber = button.getAttribute('data-supplement');
+    let dialog = document.querySelector(`#edit_dialog_${supplementNumber}`);
 
-const editDialog4 = document.getElementById('edit_dialog_4');
-const editSupBtn4 = document.getElementById('edit_sup_4');
-const cancelBtn4 = document.getElementById('cancel_btn_4');
-const disBtn4 = document.getElementById('dis_sup_4');
-const saveBtn4 = document.getElementById('save_btn_4');
-
-editSupBtn4.addEventListener('click', () => {
-  editDialog4.showModal();
-});
-cancelBtn4.addEventListener('click', () => {
-  editDialog4.close();
+    button.addEventListener('click', (event) => {
+      event.preventDefault();
+      dialog.close();
+    });
+  });
 });
