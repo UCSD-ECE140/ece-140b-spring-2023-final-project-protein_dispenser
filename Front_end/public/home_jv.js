c441f78debd21b49028af81f8b3450d4091132cb
karthikv6 <44593407+karthikv6@users.noreply.github.com>
2023-06-03 09:47:00 -0700

    dispense tied to button and parameter added in servo code

--- Front_end/public/home_jv.js
+++ Front_end/public/home_jv.js
@@ -3,6 +3,38 @@ window.addEventListener('DOMContentLoaded', (event) => {
   let saveButtons = document.querySelectorAll('[id^="save_btn_"]');
   let cancelButtons = document.querySelectorAll('[id^="cancel_btn_"]');
 
+  let dispenseButtons = document.querySelectorAll('[id^="dis_sup_"]');
+
+  dispenseButtons.forEach(button => {
+    let supplementNumber = button.id.split('_')[2]; // Get the supplement number
+    let servingInput = document.querySelector(`#sup_serv_${supplementNumber}`);
+
+    button.addEventListener('click', () => {
+      // Grab the current serving size
+      let servingSize = servingInput.value;
+
+      // Send a POST request to the /dispense route
+      fetch('/dispense', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          supplementNumber: supplementNumber,
+          servingSize: servingSize
+        })
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data); // Log the response data for now, you can replace this with other actions
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        });
+    });
+  });
+
+
   editButtons.forEach(button => {
     let supplementNumber = button.getAttribute('data-supplement');
     let dialog = document.querySelector(`#edit_dialog_${supplementNumber}`);
@@ -35,4 +67,6 @@ window.addEventListener('DOMContentLoaded', (event) => {
       dialog.close();
     });
   });
+
+  
 });
---
+++ hx711py/run_until_weigth.py
@@ -0,0 +1,70 @@
+#! /usr/bin/python2
+
+import time
+import sys
+import argparse
+
+EMULATE_HX711 = False
+
+referenceUnit = -487.11
+
+if not EMULATE_HX711:
+    import RPi.GPIO as GPIO
+    from hx711 import HX711
+else:
+    from emulated_hx711 import HX711
+
+
+def cleanAndExit():
+    print("Cleaning...")
+    motor.stop()
+
+    if not EMULATE_HX711:
+        GPIO.cleanup()
+
+    print("Bye!")
+    sys.exit()
+
+
+# Add argparse for command line arguments
+parser = argparse.ArgumentParser(description='Weight measurement script.')
+parser.add_argument('target_weight', metavar='N',
+                    type=int, help='The target weight')
+args = parser.parse_args()
+
+pi = pigpio.pi()
+pi.set_mode(25, pigpio.OUTPUT)
+
+print("mode: ", pi.get_mode(25))
+
+
+hx = HX711(5, 6)
+hx.set_reading_format("MSB", "MSB")
+hx.set_reference_unit(referenceUnit)
+hx.reset()
+hx.tare()
+print("Tare done! Add weight now...")
+
+target_weight = args.target_weight  # finish weight
+
+while True:
+    try:
+        val = hx.get_weight(5)
+        print(val)
+
+        if val >= target_weight:
+            print("Target weight reached!")
+            break
+
+        print("setting to: ", pi.set_servo_pulsewidth(25, 1000))
+        print("set to: ", pi.get_servo_pulsewidth(25))
+
+        time.sleep(1)
+
+        print("setting to: ", pi.set_servo_pulsewidth(25, 2000))
+        print("set to: ", pi.get_servo_pulsewidth(25))
+
+        time.sleep(1)
+
+    except (KeyboardInterrupt, SystemExit):
+        cleanAndExit()
